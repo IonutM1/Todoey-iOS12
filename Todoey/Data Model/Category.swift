43bc59953f7c04c76c4eb15f22e4cded2a8b1cbd
Ionut Marinica <ionut@macbook-pro-ionut.local>
2019-01-27 20:38:22 +0200

    Persisting data using Realm

---
+++ Todoey/Data Model/Category.swift
@@ -0,0 +1,16 @@
+//
+//  Category.swift
+//  Todoey
+//
+//  Created by Ionut Marinica on 27/01/2019.
+//  Copyright © 2019 Ionut Marinica. All rights reserved.
+//
+
+import Foundation
+import RealmSwift
+
+
+class Category: Object {
+    @objc dynamic var name: String = ""
+    let items = List<Item>()
+}
